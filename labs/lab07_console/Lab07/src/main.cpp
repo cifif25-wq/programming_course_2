fbadc3da6392424546f50dea09cd172854b11709
cifif25-wq <cifif25@gmail.com>
2026-02-16 22:57:59 +0700

    AA
    asasdasd

--- labs/lab07_console/Lab07/src/main.cpp
+++ labs/lab07_console/Lab07/src/main.cpp
@@ -1,31 +1,69 @@
 #include <iostream>
+using namespace std;
 
-// Lab 07
-// TODO: реализуйте решение по заданию в labs/lab07_console/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
-int main() {
-    // Задание 1: вычисление значений функции.
-    // Ввод: x0 — начальная точка интервала.
-    double x0 = 0.0;
-    if (!(std::cin >> x0)) {
-        return 0;
+#define S 10
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int s1[S];
+    for (int i = 0; i < 10; i++) 
+        s1[i] = i * i;
+
+    cout << "Статистический без указателя: ";
+    for (int i = 0; i < 10; i++)  
+        cout << s1[i] <<" ";
+    
+    cout << "\n";
+
+    int s2[S] ;
+    int* w = s2;
+
+    for (int i = 0; i < 10; i++) {
+        *w = i * i;
+        w++;
     }
-    // dfdfdf
-    // // dfd/df
-    // /dfdfd
-    // TODO: вычислите и выведите 10 значений функции на (0, 4].
-
-    // Задание 2: сумма чисел Фибоначчи.
-    // Ввод: MAX — ограничение на сумму.
-    long long max_value = 0;
-    if (!(std::cin >> max_value)) {
-        return 0;
+    
+    w = s2;
+    cout << "Статистический c указателем: ";
+    for (int i = 0; i < 10; i++) {
+        cout << *w << " ";
+        w++;
     }
 
-    // TODO: вычислите N и S (S <= MAX) и выведите результаты.
+    cout << "\n";
+
+    int* s3;
+    s3 = new int[S];
+
+    for (int i = 0; i < 10; i++) 
+        s3[i] = i * i;
+    
+    cout << "Динамический без указателя: ";
+    for (int i = 0; i < 10; i++) 
+        cout << s3[i] <<" ";
+    
+
+    cout << "\n";
+    delete[] s3;
+
+    int* s4;
+    s4 = new int[S];
+    int* w2 = s4;
+
+    for (int i = 0; i < 10; i++) {
+        *w2 = i * i;
+        w2++;
+    }
+
+    w2 = s4;
+    cout << "Динамический с указателем: ";
+    for (int i = 0; i < 10; i++) {
+        cout << *w2 <<" ";
+        w2++;
+    }
+
+    delete[] s4;
+
     return 0;
 }
